355459e15d5514534092fdd04497c79440d76d0e
Ryan Leavitt <hopelessgains@gmail.com>
2021-04-09 20:23:53 -0600

    added email component

---
+++ src/components/Auth.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { getUser } from '../redux/userReducer'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
+
+const Auth = ({ getUser }) => {
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  })
+
+  const history = useHistory()
+
+  const loginUser = async (e) => {
+    e.preventDefault()
+    const { email, paswword } = state
+
+    try {
+      const user = await axios.post('/auth/login', { email, paswword })
+      getUser(user.data)
+      history.push('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const changeHandler = e => setState({
+    ...state,
+    [e.target.name]: e.target.value
+  })
+  return (
+    <div className='auth-component'>
+      <h1 className='auth-title'>Sign In</h1>
+      <form className='auth-sign-in' onSubmit={(e => loginUser(e))}>
+        <input
+          className='auth-input-email'
+          placeholder='Hopless Gains ID'
+          name='email'
+          onChange={(e) => changeHandler(e)}
+        />
+        <input
+          className='auth-inout-password'
+          placeholder='Password'
+          name='password'
+          onChange={(e) => changeHandler(e)}
+        />
+        <section>
+          <p className='auth-reminder'>Your Hopeless Gains ID is your email you used to register</p>
+        </section>
+        <button className='sign-in-button' type='submit'>Sign in</button>
+      </form>
+      <p className='auth-forgot'>Forgot your email or password</p>
+      <Link className='registration-link' to={'/register'}>No Hopeless Gains ID? Create one</Link>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => state;
+export default connect(mapStateToProps, { getUser })(Auth)
--- src/components/Register.js
+++ src/components/Register.js
@@ -41,74 +41,69 @@ const Register = ({ getUser }) => {
     }
   }
 
-const changeHandler = e => setState({ ...state, [e.target.name]: e.target.value })
+  const changeHandler = e => setState({ ...state, [e.target.name]: e.target.value })
 
-return (
-  <div>
-    <div className="register-nav-bar">
-      <div className="register-nav-bar-left">
-        <h3> Email </h3>
+  return (
+    <div>
+      <div className="register-nav-bar">
+        <div className="register-nav-bar-left">
+          <h3> Email </h3>
+        </div>
+        <div className="register-nav-bar-right">
+          <h6>Sign In</h6>
+          <h6>Create Your Account</h6>
+        </div>
       </div>
-      <div className="register-nav-bar-right">
-        <h6>Sign In</h6>
-        <h6>Create Your Account</h6>
+      <div className="register-header">
+        <h1 className="register-header-text">Create Your Hopeless Gains Account </h1>
       </div>
+      <form onSubmit={(e) => registerUser(e)}>
+        <div className="register-form-text">
+          <p>Already have a Hopeless Gains Account? Find it here</p>
+        </div>
+        <div className="register-name-input">
+          <input
+            className="register-first-name"
+            placeholder="First name"
+            onChange={(e) => changeHandler(e)}
+            name='firstName'
+          />
+          <input
+            className="register-last-name"
+            placeholder="Last name"
+            onChange={(e) => changeHandler(e)}
+            name="lastName"
+          />
+        </div>
+        <div className="register-email-input">
+          <input
+            placeholder="name@example.com"
+            onChange={(e) => changeHandler(e)}
+            name='email'
+          />
+          <input
+            placeholder="Password"
+            onChange={(e) => changeHandler(e)}
+            name='password'
+          />
+        </div>
+        <div className="register-phone-input">
+          <input
+            placeholder="Phone number"
+            type="number"
+            onChange={(e) => changeHandler(e)}
+            name='phoneNumber'
+          />
+        </div>
+        <div className="register-checkbox">
+          <p>Verify with:</p>
+          <input className="checkbox-email-message" type="checkbox" />
+          <p>Email message</p>
+        </div>
+        <button className='register-button' type='submit'>Continue</button>
+      </form>
     </div>
-    <div className="register-header">
-      <h1 className="register-header-text">Create Your Hopeless Gains Account </h1>
-    </div>
-    <form onSubmit={(e) => registerUser(e)}>
-      <div className="register-form-text">
-        <p>Already have a Hopeless Gains Account? Find it here</p>
-      </div>
-      <div className="register-name-input">
-        <input
-          className="register-first-name"
-          placeholder="First name"
-          onChange={(e) => changeHandler(e)}
-          name='firstName'
-        />
-        <input
-          className="register-last-name"
-          placeholder="Last name"
-          onChange={(e) => changeHandler(e)}
-          name="lastName"
-        />
-      </div>
-      <div className="register-email-input">
-        <input
-          placeholder="name@example.com"
-          onChange={(e) => changeHandler(e)}
-          name='email'
-        />
-        <input
-          placeholder="Password"
-          onChange={(e) => changeHandler(e)}
-          name='password'
-        />
-      </div>
-      <div className="register-phone-input">
-        <input
-          placeholder="Phone number"
-          type="number"
-          onChange={(e) => changeHandler(e)}
-          name='phoneNumber'
-        />
-      </div>
-      <p>
-        Be sure to enter a phone number you can always access. It will be used
-        to verify your identity any time you sign in on a new device or web
-        browser. Messaging or data rates may apply.
-        </p>
-      <div className="register-checkbox">
-        <p>Verify with:</p>
-        <input className="checkbox-text-message" type="checkbox" />
-        <p>Text message</p>
-      </div>
-      <button className='register-button' type='submit'>Continue</button>
-    </form>
-  </div>
-);
+  );
 }
 const mapStateToProps = (state) => state
 export default connect(mapStateToProps, { getUser })(Register);
